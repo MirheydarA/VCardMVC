bf0985946d23e32fcdffdd3c5367f361e0b6f25a
MirheyderA <a.mirheyder004@gmail.com>
2024-08-26 23:51:49 +0400

    vCARD is finished

---
+++ VCardWithQr/Controllers/VCardController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+using System.Drawing.Imaging;
+using VCardWithQr.Models;
+using ZXing;
+using ZXing.Common;
+
+namespace VCardWithQr.Controllers;
+
+public class VCardController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult GenerateQR(VCard model)
+    {
+
+        var vcardText = model.ToVCard();
+
+        // Initialize a barcode writer
+        var writer = new BarcodeWriterPixelData
+        {
+            Format = BarcodeFormat.QR_CODE,
+            Options = new EncodingOptions
+            {
+                Height = 300,
+                Width = 300
+            }
+        };
+
+        var pixelData = writer.Write(vcardText);
+
+        using (var bitmap = new Bitmap(pixelData.Width, pixelData.Height, PixelFormat.Format32bppRgb))
+        {
+            using (var ms = new MemoryStream())
+            {
+                var bitmapData = bitmap.LockBits(new Rectangle(0, 0, pixelData.Width, pixelData.Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppRgb);
+                try
+                {
+                    System.Runtime.InteropServices.Marshal.Copy(pixelData.Pixels, 0, bitmapData.Scan0, pixelData.Pixels.Length);
+                }
+                finally
+                {
+                    bitmap.UnlockBits(bitmapData);
+                }
+
+                bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+
+                var qrCodeBase64 = $"data:image/png;base64,{Convert.ToBase64String(ms.ToArray())}";
+
+                ViewBag.QRCodeImage = qrCodeBase64;
+            }
+        }
+
+        return View("Index", model);
+    }
+}
---
+++ VCardWithQr/Models/VCard.cs
@@ -0,0 +1,23 @@
+﻿namespace VCardWithQr.Models;
+
+public class VCard
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+    public string Company { get; set; }
+
+    public string ToVCard()
+    {
+        return $"BEGIN:VCARD\n" +
+               $"VERSION:3.0\n" +
+               $"FN:{FirstName} {LastName}\n" +
+               $"ORG:{Company}\n" +
+               $"TEL;TYPE=WORK,VOICE:{PhoneNumber}\n" +
+               $"ADR;TYPE=WORK:;;{Address}\n" +
+               $"EMAIL:{Email}\n" +
+               $"END:VCARD";
+    }
+}
